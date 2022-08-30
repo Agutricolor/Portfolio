ff1ba6d288f286bb64e075528061894916e99710
Agustin Vazquez <agustinvsexto196@gmail.com>
2022-08-30 14:49:13 -0300

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import {Link} from "react-scroll"
+
+export const Navbar = () => {
+
+    return (
+            <nav className="bg-sky-700 w-full h-24 flex items-center justify-around font-mono text-xl font-semibold text-gray-200 shadow-lg shadow-blue-700/50">
+                <a href="#">Agustin Vazquez</a>
+                <div>
+                    <div id="NavLinks" className="duration-700">
+                        <a href="#" className="px-8 hover:underline underline-offset-8"><Link activeClass="px-8 underline underline-offset-8" to="Sobre mí">Sobre mí</Link></a>
+                        <a href="#" className="px-8 hover:underline underline-offset-8"><Link activeClass="px-8 underline underline-offset-8" to="Habilidades">Habilidades</Link></a>
+                        <a href="#" className="px-8 hover:underline underline-offset-8"><Link activeClass="px-8 underline underline-offset-8" to="Proyectos">Proyectos</Link></a>
+                        <a href="#" className="px-8 border-solid border-2 rounded-md p-2 mx-2 hover:bg-slate-200 hover:text-sky-700 duration-500"><Link to="Contacto">Contacto</Link></a>
+                        <a href="#" className="px-8 border-solid border-2 rounded-md p-2 mx-2 hover:bg-slate-200 hover:text-sky-700 duration-500">Descargar CV</a>
+                    </div>
+                </div>
+            </nav>
+    )
+}
